975b7285769ca1b69a783607715f1b9d73db53b9
dxg <15116029307@163.com>
2025-11-04 19:53:28 +0800

    修改订阅公众号的返回内容

--- paicoding-web/src/main/java/com/github/paicoding/forum/web/front/login/wx/helper/WxAckHelper.java
+++ paicoding-web/src/main/java/com/github/paicoding/forum/web/front/login/wx/helper/WxAckHelper.java
@@ -42,13 +42,14 @@ public class WxAckHelper {
         if (("subscribe".equalsIgnoreCase(eventType) || "scan".equalsIgnoreCase(eventType))
                 && !CodeGenerateUtil.isVerifyCode(content)) {
             // 单纯的服务号订阅、扫码，而不是登录的场景，返回下面的提示信息
-            textRes = "优秀的你一关注，二哥英俊的脸上就泛起了笑容。我这个废柴，既可以把程序人生写得风趣幽默，也可以把技术文章写得通俗易懂。\n" +
-                    "\n" +
-                    "可能是 2023 年最硬核的面试学习资料，内容涵盖 Java、Spring、MySQL、Redis、计算机网络、操作系统、消息队列、分布式等，60 万+字，300 张+手绘图，GitHub 星标 9000+，相信一定能够帮助到你。\n" +
-                    "\n" +
-                    "<a href=\"https://mp.weixin.qq.com/s/kGmZpxeV3lj04yjA0KRUXA\">[勾引]PDF 戳这里获取，手慢无！</a>\n" +
-                    "\n" +
-                    "没有什么使我停留——除了目的，纵然岸旁有玫瑰、有绿荫、有宁静的港湾，我是不系之舟。\n";
+//            textRes = "优秀的你一关注，二哥英俊的脸上就泛起了笑容。我这个废柴，既可以把程序人生写得风趣幽默，也可以把技术文章写得通俗易懂。\n" +
+//                    "\n" +
+//                    "可能是 2023 年最硬核的面试学习资料，内容涵盖 Java、Spring、MySQL、Redis、计算机网络、操作系统、消息队列、分布式等，60 万+字，300 张+手绘图，GitHub 星标 9000+，相信一定能够帮助到你。\n" +
+//                    "\n" +
+//                    "<a href=\"https://mp.weixin.qq.com/s/kGmZpxeV3lj04yjA0KRUXA\">[勾引]PDF 戳这里获取，手慢无！</a>\n" +
+//                    "\n" +
+//                    "没有什么使我停留——除了目的，纵然岸旁有玫瑰、有绿荫、有宁静的港湾，我是不系之舟。\n";
+            textRes = "欢迎订阅我的频道，有啥好吃的欢迎投稿！！！";
         }
         // 下面是关键词回复
         else if (chatgptService.inChat(fromUser, content)) {
